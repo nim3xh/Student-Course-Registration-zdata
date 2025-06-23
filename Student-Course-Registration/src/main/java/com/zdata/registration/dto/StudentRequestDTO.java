b6fef51d24041a96a8d1623f2c9294bcfd8f4550
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-23 15:35:04 +0530

    Add validation annotations to StudentRequestDTO for name and email fields

--- Student-Course-Registration/src/main/java/com/zdata/registration/dto/StudentRequestDTO.java
+++ Student-Course-Registration/src/main/java/com/zdata/registration/dto/StudentRequestDTO.java
@@ -1,4 +1,36 @@
 package com.zdata.registration.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class StudentRequestDTO {
+    // Name should not be blank and must be between 2 and 100 characters
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    // Email should be a valid email format
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
