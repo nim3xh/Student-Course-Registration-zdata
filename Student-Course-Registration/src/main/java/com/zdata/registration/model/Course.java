f5c0b6cf6da93b4f4756e24d1d215ca7d85c0545
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-23 14:21:31 +0530

    Add Course class with fields for id, name, and email, including getter and setter methods

--- Student-Course-Registration/src/main/java/com/zdata/registration/model/Course.java
+++ Student-Course-Registration/src/main/java/com/zdata/registration/model/Course.java
@@ -1,4 +1,33 @@
 package com.zdata.registration.model;
 
+import java.util.UUID;
+
 public class Course {
+    private UUID id;
+    private String name;
+    private String emaill;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmaill() {
+        return emaill;
+    }
+
+    public void setEmaill(String emaill) {
+        this.emaill = emaill;
+    }
 }
