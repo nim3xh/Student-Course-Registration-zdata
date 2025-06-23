6c50f5e52ef9182a02ea0fff9b5d71e170d64f29
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-23 14:19:18 +0530

    Refactor Student class to include getter and setter methods for id, name, and email fields

--- Student-Course-Registration/src/main/java/com/zdata/registration/model/Student.java
+++ Student-Course-Registration/src/main/java/com/zdata/registration/model/Student.java
@@ -7,4 +7,28 @@ public class Student {
     private String name;
     //should be unique
     private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
