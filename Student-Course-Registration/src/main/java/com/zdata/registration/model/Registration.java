b6a2ee613c6f3b954c287f1c8579287d63a83b17
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-23 14:23:15 +0530

    Add fields for studentId, courseId, and registeredAt to Registration class

--- Student-Course-Registration/src/main/java/com/zdata/registration/model/Registration.java
+++ Student-Course-Registration/src/main/java/com/zdata/registration/model/Registration.java
@@ -1,4 +1,12 @@
 package com.zdata.registration.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Registration {
+    private UUID studentId;
+    private UUID courseId;
+    private LocalDateTime registeredAt;
+
+    
 }
