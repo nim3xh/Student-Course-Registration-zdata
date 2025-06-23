a2c387f5dee930ca0434e092e5e94b1a35e99ba6
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-23 15:36:33 +0530

    Implement StudentResponseDTO class with constructor, getters, and setters

--- Student-Course-Registration/src/main/java/com/zdata/registration/dto/StudentResponseDTO.java
+++ Student-Course-Registration/src/main/java/com/zdata/registration/dto/StudentResponseDTO.java
@@ -1,4 +1,43 @@
 package com.zdata.registration.dto;
 
+import java.util.UUID;
+
 public class StudentResponseDTO {
+    private UUID id;
+    private String name;
+    private String email;
+
+    // constructor, getters, setters
+    public StudentResponseDTO(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    //getters and setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
