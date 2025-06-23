5020a72a72c9dddec5d2ba973c5e9f3ae8bbd156
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-06-23 15:57:15 +0530

    Implement CourseController with endpoints for adding and retrieving courses

--- Student-Course-Registration/src/main/java/com/zdata/registration/controller/CourseController.java
+++ Student-Course-Registration/src/main/java/com/zdata/registration/controller/CourseController.java
@@ -1,4 +1,31 @@
 package com.zdata.registration.controller;
 
+import com.zdata.registration.dto.*;
+import com.zdata.registration.service.CourseService;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseResponseDTO> addCourse(@Valid @RequestBody CourseRequestDTO request) {
+        return ResponseEntity.ok(courseService.addCourse(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseResponseDTO>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
 }
